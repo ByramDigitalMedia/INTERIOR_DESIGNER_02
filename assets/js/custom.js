452f43295abf26d4fdc7e2c45e7c5b59cd81ddd2
ByramDigitalMedia <contact@byramdigitalmedia.com>
2025-11-07 18:40:12 -0500

    first commit

---
+++ assets/js/custom.js
@@ -0,0 +1,23 @@
+const header = document.querySelector("header")
+const sectionOne = document.querySelector(".change-name")
+
+const sectionOneOptions = {
+  rootMargin: "-200px 0px 0px 0px"
+}
+
+const sectionOneObserver = new IntersectionObserver(function (entries, sectionOneObserver) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      header.classList.add("nav-scrolled")
+    } else {
+      header.classList.remove("nav-scrolled")
+    }
+  })
+}, sectionOneOptions)
+
+sectionOneObserver.observe(sectionOne)
+
+// **********************************************
+
+let date = new Date().getFullYear()
+document.getElementById("year").innerHTML = date
